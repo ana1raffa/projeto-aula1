897484a070dc279ffc17a798dce7ae392fced8b8
ana1raffa <ana.raffa@fatec.sp.gov.br>
2025-02-26 19:02:28 -0300

    arrumando o controller

--- src/main/java/com/fatec/projeto/projeto2025/controllers/exercicioController.java
+++ src/main/java/com/fatec/projeto/projeto2025/controllers/exercicioController.java
@@ -2,7 +2,7 @@ package com.fatec.projeto.projeto2025.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 
@@ -12,4 +12,10 @@ public class exercicioController {
     public String Helloworld(){
         return "Hello";
     }
+
+     @GetMapping("{nome}")
+    public String Helloworld(@PathVariable Optional<String> nome){
+        return nome.isPresent () ? nome.get(): "dd";
+    }
+
 }
