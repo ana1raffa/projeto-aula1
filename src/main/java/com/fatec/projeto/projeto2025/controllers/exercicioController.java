7189b76f0e04c2b0c99e552a0c96bd399067076a
ana1raffa <ana.raffa@fatec.sp.gov.br>
2025-02-27 21:04:57 -0300

    exercicio condicional idade

--- src/main/java/com/fatec/projeto/projeto2025/controllers/exercicioController.java
+++ src/main/java/com/fatec/projeto/projeto2025/controllers/exercicioController.java
@@ -1,5 +1,7 @@
 package com.fatec.projeto.projeto2025.controllers;
 
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,15 +9,31 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RestController
 
 public class exercicioController {
-
     @GetMapping("")
-    public String Helloworld(){
-        return "Hello";
+    public String HelloWorld(){
+        return "Hello";    
+    
     }
 
-     @GetMapping("{nome}")
-    public String Helloworld(@PathVariable Optional<String> nome){
-        return nome.isPresent () ? nome.get(): "dd";
+
+    @GetMapping("{idade}")
+    public String VerificarIdade(@PathVariable Optional<String> idade){
+        idade.ifPresent(value -> System.out.println("Idade: " + value)); 
+        int idadeInt = Integer.parseInt(idade.get());
+        if (idadeInt > 0 && idadeInt < 12 ){
+            return "Criança";
+        } else if (idadeInt >= 12 && idadeInt <=18){
+            return "Adolescente";
+        }else if (idadeInt >= 19 && idadeInt <=60){
+            return "Adulto";
+        }else if (idadeInt > 60){
+            return "Idoso";
+        } else{
+            return "Idade Inválida";
+        }
+     
+    
     }
+  
 
 }
