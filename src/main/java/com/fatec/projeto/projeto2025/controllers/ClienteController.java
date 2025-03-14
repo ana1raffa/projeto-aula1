a47cf3192982175cf54ec5e783ac8025042c1fa4
ana1raffa <ana.raffa@fatec.sp.gov.br>
2025-03-13 22:31:13 -0300

    criado a funcao de deletar e atualizar cliente

--- src/main/java/com/fatec/projeto/projeto2025/controllers/ClienteController.java
+++ src/main/java/com/fatec/projeto/projeto2025/controllers/ClienteController.java
@@ -4,9 +4,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fatec.projeto.projeto2025.entities.Cliente;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -16,10 +24,50 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class ClienteController {
         private static final Logger logger = LoggerFactory.getLogger(ClienteController.class.getName());
 
+        private final List<Cliente> clientes = new ArrayList<>();
+        private Long idCount = 1L; 
+
+
     //http://localhost:8080/api/cliente/criarCliente => POST
     @PostMapping("/criarCliente")
     public String CriarCliente(@RequestBody Cliente cliente) {
+        cliente.setId(idCount++);
+        clientes.add(cliente);
+
         logger.info("Recebido JSON: Nome={}, Idade={}", cliente.getNome(), cliente.getIdade());
         return "O Cliente "+cliente.getNome()+ " de idade "+cliente.getIdade()+" foi criado";
     }
-}
+    @GetMapping ("/listarClientes")
+    public List<Cliente> ListarClientes(){
+        return clientes;
+    }
+
+    
+    @DeleteMapping("/deletarCliente/{id}")
+    public String DeletarClientes(@PathVariable Long id){
+        for (Cliente cliente: clientes){
+            if (cliente.getId().equals(id)){
+                clientes.remove(cliente);
+                return "Cliente removido com sucesso!";
+            }
+        }
+        return "nao existe cliente com id: "+id;
+    }
+
+    @PutMapping("/atualizarCliente/{id}")
+    public String AtualizarCliente(@PathVariable Long id, @RequestBody Cliente cliente){
+        for(Cliente c: clientes){
+            if(c.getId().equals(id)){
+                c.setNome(cliente.getNome());
+                c.setIdade(cliente.getIdade());
+                return "Cliente atualizado com sucesso";
+            }
+        }
+        return "Não existe um cliente cadastrado com o id: "+id;
+    }
+    }
+
+
+
+
+
